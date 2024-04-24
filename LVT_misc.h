29da77fab16d8475c400d96436205b41b2b16347
fgr81 <fgr81@hotmail.it>
2024-04-24 11:56:19 +0000

    fix missed _misc.h

---
+++ LVT_misc.h
@@ -0,0 +1,12 @@
+#define USE_NETCDF4 
+#define USE_GRIBAPI 
+#define USE_ECCODES 
+#define USE_HDF4 
+#define USE_HDFEOS2 
+#define USE_HDF5 
+#undef AFWA_GRIB_CONFIGS 
+#undef USE_MATLAB_SUPPORT 
+#define USE_GDAL 
+#undef INC_WATER_PTS
+#undef USE_GRIB2
+#undef SPMD
